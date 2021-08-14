ace6dddc0ace1a28bf14fb97ac55a14ea7d7e802
moritzwinger <moritzwinger@users.noreply.github.com>
2021-08-14 09:58:58 +0000

    deploy: 2ea2b2ff555d073908252b713483fb24066afe4a

--- search/all_e.js
+++ search/all_e.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['readme_248',['README',['../md__r_e_a_d_m_e.html',1,'']]],
   ['registerbeforevisitchildren_249',['registerBeforeVisitChildren',['../class_special_type_checking_visitor.html#a9254188674777ceb87ea5ec14aec60b7',1,'SpecialTypeCheckingVisitor']]],
-  ['removemodswitchfromast_250',['removeModSwitchFromAst',['../class_special_insert_mod_switch_visitor.html#a96c0d889726534d3e81c0cf9cec0af50',1,'SpecialInsertModSwitchVisitor']]],
+  ['removemodswitchfromast_250',['removeModSwitchFromAst',['../class_special_insert_mod_switch_visitor.html#a58388dc3b04278bad6427878f8ef0829',1,'SpecialInsertModSwitchVisitor']]],
   ['removenullexpressions_251',['removeNullExpressions',['../class_expression_list.html#ac61f7f1660da540c582b3fac789d260d',1,'ExpressionList']]],
   ['removenulloperands_252',['removeNullOperands',['../class_operator_expression.html#ae39ca6dfb6c6faba090742a3c0a7d7bc',1,'OperatorExpression']]],
   ['removenullstatements_253',['removeNullStatements',['../class_block.html#ac5cce2f43038e00f090e2d79666c7f80',1,'Block']]],
@@ -11,7 +11,7 @@ var searchData=
   ['return_256',['Return',['../class_return.html',1,'Return'],['../class_return.html#ad9462245d0d13b95ab0a85e16c80b835',1,'Return::Return(std::unique_ptr&lt; AbstractExpression &gt; value=nullptr)'],['../class_return.html#aff6b3b7999bd8cb14096c5fafe601f42',1,'Return::Return(const Return &amp;other)'],['../class_return.html#a754af1b64a46fe55834eeb73151cce14',1,'Return::Return(Return &amp;&amp;other) noexcept']]],
   ['returniteratorimpl_257',['ReturnIteratorImpl',['../class_return_iterator_impl.html',1,'']]],
   ['returnstatementreached_258',['ReturnStatementReached',['../struct_return_statement_reached.html',1,'']]],
-  ['rewriteast_259',['rewriteAst',['../class_cone_rewriter.html#a5f5c8badb41d945cb36a38ceb941312c',1,'ConeRewriter::rewriteAst()'],['../class_special_insert_mod_switch_visitor.html#a56fd31a8eefdccb511a8378b9000c5cc',1,'SpecialInsertModSwitchVisitor::rewriteAst()']]],
+  ['rewriteast_259',['rewriteAst',['../class_cone_rewriter.html#a5f5c8badb41d945cb36a38ceb941312c',1,'ConeRewriter::rewriteAst()'],['../class_special_insert_mod_switch_visitor.html#afe5af723e743e28e7f000c49962a29c2',1,'SpecialInsertModSwitchVisitor::rewriteAst()']]],
   ['rewritecones_260',['rewriteCones',['../class_cone_rewriter.html#a6c1bfa4d386b84deafd1e1e1af71a3be',1,'ConeRewriter']]],
   ['rotaterows_261',['rotateRows',['../class_abstract_ciphertext.html#a10939e806fad43fc8d7771cd9b42f0b7',1,'AbstractCiphertext::rotateRows()'],['../class_dummy_ciphertext.html#aa88a537fb8829609c3b078e19d43cd9f',1,'DummyCiphertext::rotateRows()']]],
   ['rotaterowsinplace_262',['rotateRowsInplace',['../class_abstract_ciphertext.html#af8eff555a247b14531f2509a787e4491',1,'AbstractCiphertext::rotateRowsInplace()'],['../class_dummy_ciphertext.html#a781ec3287d27f8a97cece047e0b4beaf',1,'DummyCiphertext::rotateRowsInplace()']]],
